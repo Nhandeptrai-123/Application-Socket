5146d3448555a1d5cf0869aeeda184d0a97a10dd
gialiem130 <you@example.com>
2024-06-24 22:11:31 +0700

    add package cameraStuff in ui package to code CameraScreen

---
+++ app/src/main/java/com/example/applicationsocket/ui/cameraStuff/CameraScreenTakePicture.kt
@@ -0,0 +1,19 @@
+package com.example.applicationsocket.ui.cameraStuff
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.applicationsocket.Greeting
+import com.example.applicationsocket.ui.theme.ApplicationSocketTheme
+
+@Composable
+fun TopBar(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    ApplicationSocketTheme {
+        TopBar()
+    }
+}
